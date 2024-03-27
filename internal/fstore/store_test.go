a05eb8d1765b9844d04eab87a61db5d42e63b25d
John Hughes <john.hughes@musictribe.com>
2024-03-27 12:09:34 +0000

    feat: :sparkles: add GetFile method

---
+++ internal/fstore/store_test.go
@@ -0,0 +1,36 @@
+package fstore
+
+import (
+	"bytes"
+	"io"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestStore(t *testing.T) {
+	t.Run("test store lifecycle", func(t *testing.T) {
+		store, err := NewStore("./tmp")
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		filepath := "some/path/to/file.txt"
+
+		fileData := []byte("some file data")
+		err = store.SaveFile(filepath, bytes.NewBuffer(fileData))
+		require.NoError(t, err)
+
+		file, err := store.GetFile(filepath)
+		require.NoError(t, err)
+
+		byt, err := io.ReadAll(file)
+		require.NoError(t, err)
+
+		err = file.Close()
+		require.NoError(t, err)
+
+		assert.Equal(t, fileData, byt)
+	})
+}
