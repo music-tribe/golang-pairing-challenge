975f1b68c9810eb1b2d62379230ba8c74e991ba7
John Hughes <john.hughes@musictribe.com>
2024-03-27 12:30:01 +0000

    refactor: :recycle: register routes

--- httprest/api.go
+++ httprest/api.go
@@ -15,18 +15,22 @@ type API struct {
 }
 
 func NewAPI(svc ports.Service) *API {
-	return &API{
+	api := &API{
 		svc:    svc,
 		router: mux.NewRouter(),
 	}
+
+	api.RegisterRoutes()
+
+	return api
 }
 
 func (a *API) RegisterRoutes() {
 	// new routes
-	a.router.HandleFunc("api/files/{user_id}/{id}", getfile.Handler(a.svc)).Methods("GET")
+	a.router.HandleFunc("/api/files/{user_id}/{id}", getfile.Handler(a.svc)).Methods("GET")
 
 	// lagacy routes
-	a.router.HandleFunc("api/files/{user_id}", a.svc.UploadFile).Methods("POST")
+	a.router.HandleFunc("/api/files/{user_id}", a.svc.UploadFile).Methods("POST")
 }
 
 func (a *API) Run(port string) error {
