3c7136865a528f4e3d94db8f56f5746cbd8f68a7
John Hughes <john.hughes@musictribe.com>
2024-03-27 11:41:41 +0000

    feat: :sparkles: add getFile handler

---
+++ httprest/getfile/handler.go
@@ -0,0 +1,64 @@
+package getfile
+
+import (
+	"io"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/music-tribe/golang-pairing-challenge/ports"
+	"github.com/music-tribe/uuid"
+)
+
+type Request struct {
+	Id     uuid.UUID `validate:"required"`
+	UserId uuid.UUID `validate:"required"`
+}
+
+func Handler(svc ports.Service) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		req := Request{}
+		vars := mux.Vars(r)
+		if err := bindRequest(vars, &req); err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		file, err := svc.GetFile(ports.GetFileRequest{
+			Id:     req.Id,
+			UserId: req.UserId,
+		})
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		defer file.Close()
+
+		if err := sendResponse(w, file); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	}
+}
+
+func bindRequest(vars map[string]string, req *Request) (err error) {
+	req.Id, err = uuid.Parse(vars["id"])
+	if err != nil {
+		return err
+	}
+
+	req.UserId, err = uuid.Parse(vars["user_id"])
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func sendResponse(w http.ResponseWriter, file io.Reader) error {
+	if _, err := io.Copy(w, file); err != nil {
+		return err
+	}
+
+	w.WriteHeader(http.StatusOK)
+	return nil
+}
---
+++ ports/service.go
@@ -0,0 +1,22 @@
+package ports
+
+import (
+	"io"
+	"net/http"
+
+	"github.com/music-tribe/uuid"
+)
+
+//go:generate mockgen -destination=./../service/mocksvc/mock.go -package=mocksvc -source=service.go
+type Service interface {
+	// new actions
+	GetFile(GetFileRequest) (io.ReadCloser, error)
+
+	// leagacy handlers
+	UploadFile(w http.ResponseWriter, r *http.Request) // TODO: refactor me!!!
+}
+
+type GetFileRequest struct {
+	Id     uuid.UUID `validate:"required"`
+	UserId uuid.UUID `validate:"required"`
+}
